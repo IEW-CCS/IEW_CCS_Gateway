abfda1aaf7a26c55a2cc09bfe9ed0b225ddd3dd5
英璟 陳 <e30803@hotmail.com>
2019-05-30 11:38:43 +0800

    20190530-1 Initial EDC Manager.cs

--- IEW.EDCService/IOTEDCService.cs
+++ IEW.EDCService/IOTEDCService.cs
@@ -25,91 +25,182 @@ namespace IEW.IOTEDCService
     public class IOTEDCService : AbstractService
     {
 
-        private string _provider = string.Empty;
-        private string _connectstring = string.Empty;
-        private string _edcpath = string.Empty;
-        private int _reportinterval = 60000;
+        /*
+        private System.Threading.Thread Thread_Timer_Check_EDC = null;
+        private System.Threading.Timer Timer_Check_EDC = null;
+        private ConcurrentQueue<Tuple<string, string>> _Write_EDC_File = new ConcurrentQueue<Tuple<string, string>>();
+        
 
-        internal static ConcurrentDictionary<string, IOT_Device> _IOT_Device = new ConcurrentDictionary<string, IOT_Device>();
+        #region EDCManager Method
+        public T EDC_Deserialize<T>(string s)
+        {
+            XmlDocument xdoc = new XmlDocument();
+            try
+            {
+                xdoc.LoadXml(s);
+                XmlNodeReader reader = new XmlNodeReader(xdoc.DocumentElement);
+                XmlSerializer ser = new XmlSerializer(typeof(T));
+                object obj = ser.Deserialize(reader);
+                return (T)obj;
+            }
+            catch
+            {
+                return default(T);
+            }
+        }
+        private void EDCManager_Stop()
+        {
+            this.Thread_Timer_Check_EDC.Abort();
+        }
+        private void EDCManager_Start(int interval)
+        {
+            if (interval == 0)
+                interval = 1000;  // Default 1s scan 一次
 
-        internal static ConcurrentDictionary<string, cls_EDC_Info> _IOT_EDC = new ConcurrentDictionary<string, cls_EDC_Info>();
-        internal static ConcurrentDictionary<string, IOT_EDC_GLS_INFO> _IOT_EDC_GlassInfo = new ConcurrentDictionary<string, IOT_EDC_GLS_INFO>();
-        internal static ConcurrentQueue<Tuple<string, string>> _Write_EDC_File = new ConcurrentQueue<Tuple<string, string>>();
+            //使用匿名方法，建立帶有參數的委派
+            this.Thread_Timer_Check_EDC = new System.Threading.Thread
+            (
+               delegate (object value)
+               {
+                   int Interval = Convert.ToInt32(value);
+                   Timer_Check_EDC = new System.Threading.Timer(new System.Threading.TimerCallback(EDCManager_TimerTask), null, 1000, Interval);
+               }
+            );
+            this.Thread_Timer_Check_EDC.Start(interval);
+        }
+        private void EDCManager_TimerTask(object timerState)
+        {
+            // 在Timer task中一直檢查是否符合EDC上報條件。
+            DateTime CurrentTime = DateTime.Now;
+            Parallel.ForEach(EDCManager.gateway_edc, e =>
+            {
+                string EDC_string = string.Empty;
+                bool condition = false;
+                switch (e.report_tpye)
+                {
+                    /*
+                    case "trigger":
+                        if (e.triggered == true)
+                            condition = true;
+                        break;
+                    case "interval":
+                        TimeSpan ts = e.LastReportDatetime - CurrentTime;
+                        double diff = ts.TotalSeconds;
+                        if (diff > e.report_interval)
+                        {
+                            condition = true;
+                            e.LastReportDatetime = CurrentTime;
+                        }
+                        break;
+                        
+                    default:
+                        condition = false;
+                        break;
+                }
 
-        internal static ConcurrentDictionary<string, ConcurrentDictionary<string, Tuple<double, double>>> _EDC_Item_Spec = new ConcurrentDictionary<string, ConcurrentDictionary<string, Tuple<double, double>>>();
+                if (condition)
+                {
+                    cls_Gateway_Info Gateway = GatewayManager.gateway_list.Where(p => p.gateway_id == e.gateway_id).FirstOrDefault();
+                    if (Gateway != null)
+                    {
+                        cls_Device_Info Device = Gateway.device_info.Where(p => p.device_name == e.device_id).FirstOrDefault();
+                        if (Device != null)
+                        {
+                            //  EDC_Report tempEDC = new EDC_Report();  // 考慮直接使用反序列畫
+                            // 直接在local 存放一個 base 的 EDC 檔案，直接返序列畫成物件進行對應
+                            var sr = new StreamReader(e.baseEDCPath);
+                            /*
+                            EDC_Report tempEDC  = EDC_Deserialize<EDC_Report>(sr.ReadToEnd());
 
-        internal static string[] item_names = {"RPM","OA","OA_S","Peak","Peak_S","PtoP","PtoP_S","CF","CF_S",
-                                               "B01","B01_S","B02","B02_S","B03","B03_S","B04","B04_S","B05","B05_S",
-                                               "B06","B06_S","B07","B07_S","B08","B08_S","B09","B09_S","B10","B10_S" };
 
 
-        private System.Threading.Timer timer_report_edc;
+                            foreach (Tuple<string, string> edctag in e.tag_info)
+                            {
+                                EDC_Report.IARYClass _data_count = tempEDC.CreateIARYClass();
+                                _data_count.ITEM_NAME = edctag.Item1;
+                                _data_count.ITEM_TYPE = "X";
+                                _data_count.ITEM_VALUE = Device.tag_info[edctag.Item2].Value;
+                            }
+                            
+                            // e.LastReportDatetime = CurrentTime;
+                            // e.triggered = false;
+                            //EDC_string = tempEDC.EDC_String_Serialize();
+                        }
+                    }
+                }
+
+
+
+                if (EDC_string != string.Empty)
+                {
+                    _Write_EDC_File.Enqueue(Tuple.Create(e.ReportEDCPath, EDC_string));
+                }
+            });
+
+            EDCManager_ReportFile();
+        }
+        public void EDCManager_ReportFile()
+        {
+            if (_Write_EDC_File.Count > 0)
+            {
+                Tuple<string, string> _msg = null;
+                while (_Write_EDC_File.TryDequeue(out _msg))
+                {
+                    string save_file_path = _msg.Item1;
+                    string EDC_Data = _msg.Item2;
+
+                    try
+                    {
+                        if (!Directory.Exists(Path.GetDirectoryName(save_file_path)))
+                        {
+                            Directory.CreateDirectory(Path.GetDirectoryName(save_file_path));
+                        }
+                        string temp_name = save_file_path + ".tmp";
+                        using (FileStream fs = System.IO.File.Open(temp_name, FileMode.Create))
+                        {
+                            using (StreamWriter EDCWriter = new StreamWriter(fs, Encoding.UTF8))
+                            {
+                                EDCWriter.Write(EDC_Data);
+                                EDCWriter.Flush();
+                                fs.Flush();
+                            }
+                        }
+
+                        if (System.IO.File.Exists(save_file_path))
+                            System.IO.File.Delete(save_file_path);
+                        while (System.IO.File.Exists(save_file_path))
+                            Thread.Sleep(1);
+                        System.IO.File.Move(temp_name, save_file_path);
+                        NLogManager.Logger.LogInfo("Service", GetType().Name, MethodInfo.GetCurrentMethod().Name + "()", string.Format("Save EDC File Successful Path: {0}.", save_file_path));
+
+                    }
+                    catch (Exception ex)
+                    {
+                        NLogManager.Logger.LogError("Service", GetType().Name, MethodInfo.GetCurrentMethod().Name + "()", string.Format("Write EDC File Faild Exception Msg : {0}. ", ex.Message));
+                    }
+                }
+            }
+
+
+
+
+        }
+        #endregion
+        */
+
+
+        private System.Threading.Timer timer_routine;
+        private int routine_interval = 1;
         private bool _run = false;
 
-
-        #region DI 
-
-        public string provider
-        {
-            get { return _provider; }
-            set { _provider = value; }
-        }
-
-        public string connectstring
-        {
-            get { return _connectstring; }
-            set { _connectstring = value; }
-        }
-
-        public string edcpath
-        {
-            get { return _edcpath; }
-            set { _edcpath = value; }
-        }
-
-        public int reportinterval
-        {
-            get { return _reportinterval; }
-            set { _reportinterval = value; }
-        }
-
-        #endregion
 
         public override bool Init()
         {
             bool ret = false;
             try
             {
-                //-------- Load DB Setting --------
-                using (IOT_DbContext db = new IOT_DbContext(provider, connectstring))
-                {
-                    var vIOT_EDC_Gls_Info_Result = db.IOT_EDC_GLS_INFO.AsQueryable().ToList();
-                    foreach (IOT_EDC_GLS_INFO _IOT_EDC_Gls_Info_key in vIOT_EDC_Gls_Info_Result)
-                    {
-                        _IOT_EDC_GlassInfo.AddOrUpdate(_IOT_EDC_Gls_Info_key.sub_eqp_id, _IOT_EDC_Gls_Info_key, (key, oldvalue) => _IOT_EDC_Gls_Info_key);
-                    }
-
-                    var vIOT_Device_Result = db.IOT_Device.AsQueryable().ToList();
-                    foreach (IOT_Device _IOT_Device_key in vIOT_Device_Result)
-                    {
-                        _IOT_Device.AddOrUpdate(_IOT_Device_key.device_id, _IOT_Device_key, (key, oldvalue) => _IOT_Device_key);
-                    }
-
-                    var vEDC_Label_Result = db.IOT_Device_EDC_LABEL.AsQueryable().ToList();
-                    foreach (IOT_Device_EDC_LABEL _EDC_Label_key in vEDC_Label_Result)
-                    {
-                        ConcurrentDictionary<string, Tuple<double, double>> _Sub_EDC_Spec = _EDC_Item_Spec.GetOrAdd(_EDC_Label_key.device_id, new ConcurrentDictionary<string, Tuple<double, double>>());
-                        _Sub_EDC_Spec.AddOrUpdate(_EDC_Label_key.data_name, Tuple.Create(_EDC_Label_key.lspec, _EDC_Label_key.uspec), (key, oldvalue) => Tuple.Create(_EDC_Label_key.lspec, _EDC_Label_key.uspec));
-                        _EDC_Item_Spec.AddOrUpdate(_EDC_Label_key.device_id, _Sub_EDC_Spec, (key, oldvalue) => _Sub_EDC_Spec);
-
-                    }
-
-
-                }
-
                 _run = true;
-                Timer_Report_EDC(_reportinterval);
-
+                Timer_Routine_Job(routine_interval);
                 NLogManager.Logger.LogInfo(LogName, GetType().Name, MethodInfo.GetCurrentMethod().Name + "()", "Initial Finished");
                ret = true;
             }
@@ -141,129 +232,35 @@ namespace IEW.IOTEDCService
         }
 
 
-        private void Timer_Report_EDC(int interval)
+        private void Timer_Routine_Job(int interval)
         {
             if (interval == 0)
                 interval = 10000;  // 10s
 
             //使用匿名方法，建立帶有參數的委派
-            System.Threading.Thread Thread_Timer_Report_EDC = new System.Threading.Thread
+            System.Threading.Thread Thread_Timer_Routine_Job = new System.Threading.Thread
             (
                delegate (object value)
                {
                    int Interval = Convert.ToInt32(value);
-                   timer_report_edc = new System.Threading.Timer(new System.Threading.TimerCallback(EDC_TimerTask), null, 1000, Interval);
+                   timer_routine = new System.Threading.Timer(new System.Threading.TimerCallback(EDC_TimerTask), null, 1000, Interval);
                }
             );
-            Thread_Timer_Report_EDC.Start(interval);
+            Thread_Timer_Routine_Job.Start(interval);
         }
 
         private void EDC_TimerTask(object timerState)
         {
-            try
-            {
-                Parallel.ForEach(_IOT_EDC, kvp =>
-                {
-                    if (kvp.Value.RPM.Count() != 0)
-                    {
-                        string result = string.Empty;
-                        string EDC_File_Name = string.Empty;
-                        result = kvp.Value.Export(kvp.Key, out EDC_File_Name);
-
-                        if (result != string.Empty && EDC_File_Name != string.Empty)
-                        {
-                           
-                            string FilePath = Path.Combine(_edcpath, EDC_File_Name);
-                            _Write_EDC_File.Enqueue(Tuple.Create(FilePath, result));
-                        }
-                        else
-                        {
-                            NLogManager.Logger.LogWarn("Service", GetType().Name, MethodInfo.GetCurrentMethod().Name + "()", string.Format("Encode EDC File Faild Device : {0}.", kvp.Key));
-                           
-                        }
-
-                    }
-
-                });
-
-                ReportEDC();
-            }
-            catch (Exception ex)
-            {
-                NLogManager.Logger.LogError("Service", GetType().Name, MethodInfo.GetCurrentMethod().Name + "()", string.Format("Encode EDC File Error Exception Msg :{0}.", ex.Message));
-            }
+            
         }
 
-        public void Test(xmlMessage InputData)
-        {
-            string topic = InputData.MQTTTopic;
-            string message = InputData.Data;
-            NLogManager.Logger.LogInfo(LogName, GetType().Name, MethodInfo.GetCurrentMethod().Name + "()", string.Format("Receive_MQTT Message Topic : {0}.", topic));
-            Handle_IOT_DEVICE_EDC_ThreadPool(message);
-        }
-
-        private void SendMQTTData(xmlMessage msg)
-        {
-            PutMessage(msg);
-        }
+        
 
         private void Handle_IOT_DEVICE_EDC_ThreadPool(string message)
         {
-            try
-            {
-                cls_EDC_Function Function = new cls_EDC_Function();
-                ThreadPool.QueueUserWorkItem(o => Function.ThreadPool_Proc(message));
-            }
-
-            catch (Exception ex)
-            {
-                NLogManager.Logger.LogError(LogName, GetType().Name, MethodInfo.GetCurrentMethod().Name + "()", ex);
-            }
+           
         }
 
-        public void ReportEDC()
-        {
-            if (_Write_EDC_File.Count > 0)
-            {
-                Tuple<string, string> _msg = null;
-                while (_Write_EDC_File.TryDequeue(out _msg))
-                {
-                    string save_file_path = _msg.Item1;
-                    string EDC_Data = _msg.Item2;
-                    try
-                    {
-                        if (!Directory.Exists(Path.GetDirectoryName(save_file_path)))
-                        {
-                            Directory.CreateDirectory(Path.GetDirectoryName(save_file_path));
-                        }
-                        string temp_name = save_file_path + ".tmp";
-                        using (FileStream fs = System.IO.File.Open(temp_name, FileMode.Create))
-                        {
-                            using (StreamWriter EDCWriter = new StreamWriter(fs, Encoding.UTF8))
-                            {
-                                EDCWriter.Write(EDC_Data);
-                                EDCWriter.Flush();
-                                fs.Flush();
-
-                            }
-                        }
-
-                        if (System.IO.File.Exists(save_file_path))
-                            System.IO.File.Delete(save_file_path);
-                        while (System.IO.File.Exists(save_file_path))
-                            Thread.Sleep(1);
-                        System.IO.File.Move(temp_name, save_file_path);
-                        NLogManager.Logger.LogInfo("Service", GetType().Name, MethodInfo.GetCurrentMethod().Name + "()", string.Format("Save EDC File Successful Path: {0}.", save_file_path));
-
-                    }
-                    catch (Exception ex)
-                    {
-                        NLogManager.Logger.LogError("Service", GetType().Name, MethodInfo.GetCurrentMethod().Name + "()", string.Format("Write EDC File Faild Exception Msg : {0}. ", ex.Message));
-                    }
-                }
-            }
-
-        }
 
 
 
--- IEW.ObjectManager/EDCManager.cs
+++ IEW.ObjectManager/EDCManager.cs
@@ -3,27 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Concurrent;
 
 namespace IEW.ObjectManager
 {
 
-    public class cls_EDC_Info
+    public class EDCPartaker
     {
         public string serial_id { get; set; }
         public string gateway_id { get; set; }
         public string device_id { get; set; }
 
-        public string report_tpye { get; set; }    //目前使用Interval 未來考慮有event trigger...
-        public double report_interval { get; set; }   // 以秒為單位
-        public DateTime LastReportDatetime { get; set; }
+        public string report_tpye { get; set; }       
+        public double report_interval { get; set; }     
+        public DateTime timestapm { get; set; }
 
-        public bool triggered { get; set; }        // 保留未來給event triger使用
-
-        public string baseEDCPath { get; set; }    //EDC 寫入相關資訊
         public string ReportEDCPath { get; set; }
-        public bool enable { get; set; }
 
+        public List<cls_EDC_Head_Item> edchead_info;
+        public List<cls_EDC_Body_Item> edcitem_info;
+
+        public EDCPartaker(cls_EDC_Info EDCInfo)
+        {
+            this.serial_id = EDCInfo.serial_id;
+            this.gateway_id = EDCInfo.gateway_id;
+            this.device_id = EDCInfo.device_id;
+
+            this.report_tpye = EDCInfo.report_tpye;
+            this.report_interval = EDCInfo.report_interval;
+            this.timestapm = DateTime.Now;
+            this.ReportEDCPath = EDCInfo.ReportEDCPath;
+
+            this.edchead_info = EDCInfo.edchead_info.ToList();
+            this.edcitem_info = new List<cls_EDC_Body_Item>();
+
+        }
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+
+
+
+
+    public class cls_EDC_Info
+    {
+        public string     serial_id   { get; set; }
+        public string     gateway_id  { get; set; }
+        public string     device_id   { get; set; }
+
+        public string     report_tpye     { get; set; }       //  trigger/interval
+        public double     report_interval { get; set; }       //  以秒為單位 
+        public DateTime   timestapm       { get; set; }
+
+        public string     ReportEDCPath   { get; set; }
+        public bool       enable          { get; set; }
+
+        //------ 設定 使用 ----- item 1 report EDC item name   items2 device tag class name;
         public List<Tuple<string, string>> tag_info = new List<Tuple<string, string>>();
+
+        //------ Report EDC 使用 -----
+        public List<cls_EDC_Head_Item> edchead_info = new List<cls_EDC_Head_Item>();
 
         public object Clone()
         {
@@ -45,6 +86,13 @@ namespace IEW.ObjectManager
         public string head_name { get; set; }
         public int length { get; set; }
         public string value { get; set; }
+    }
+
+    public class cls_EDC_Body_Item
+    {
+        public string item_name  { get; set; }
+        public string item_type  { get; set; }
+        public string item_value { get; set; }
     }
 
     public class cls_EDC_Head_Set
