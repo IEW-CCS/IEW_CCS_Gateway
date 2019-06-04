47bccacf885b7a2e4660f4a5e0c2924309e9c1af
james <robohood83@gmail.com>
2019-06-04 10:35:30 +0800

    20190604-1 test

--- IEW.GatewayService/GUI/frmEditDevice.Designer.cs
+++ IEW.GatewayService/GUI/frmEditDevice.Designer.cs
@@ -45,17 +45,15 @@
             this.btnDeviceSave = new System.Windows.Forms.Button();
             this.pnlBLE = new System.Windows.Forms.Panel();
             this.pnlPLC = new System.Windows.Forms.Panel();
-            this.gbTagSet = new System.Windows.Forms.GroupBox();
+            this.btnTagRemove = new System.Windows.Forms.Button();
+            this.btnTagAdd = new System.Windows.Forms.Button();
+            this.btnLoadTag = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.lvCalcTagList = new System.Windows.Forms.ListView();
             this.lvTagList = new System.Windows.Forms.ListView();
-            this.btnTagRemove = new System.Windows.Forms.Button();
-            this.btnTagAdd = new System.Windows.Forms.Button();
-            this.btnLoadTag = new System.Windows.Forms.Button();
             this.pnlBLE.SuspendLayout();
             this.pnlPLC.SuspendLayout();
-            this.gbTagSet.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtDeviceID
@@ -216,66 +214,6 @@
             this.pnlPLC.Size = new System.Drawing.Size(638, 150);
             this.pnlPLC.TabIndex = 18;
             // 
-            // gbTagSet
-            // 
-            this.gbTagSet.Controls.Add(this.label2);
-            this.gbTagSet.Controls.Add(this.label1);
-            this.gbTagSet.Controls.Add(this.lvCalcTagList);
-            this.gbTagSet.Controls.Add(this.lvTagList);
-            this.gbTagSet.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.gbTagSet.Font = new System.Drawing.Font("Arial Narrow", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.gbTagSet.Location = new System.Drawing.Point(27, 304);
-            this.gbTagSet.Name = "gbTagSet";
-            this.gbTagSet.Size = new System.Drawing.Size(1355, 630);
-            this.gbTagSet.TabIndex = 20;
-            this.gbTagSet.TabStop = false;
-            this.gbTagSet.Text = "Tag List";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Arial Narrow", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(25, 40);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(134, 31);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "Normal Tag:";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial Narrow", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(25, 393);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(172, 31);
-            this.label1.TabIndex = 8;
-            this.label1.Text = "Calculation Tag:";
-            // 
-            // lvCalcTagList
-            // 
-            this.lvCalcTagList.FullRowSelect = true;
-            this.lvCalcTagList.GridLines = true;
-            this.lvCalcTagList.Location = new System.Drawing.Point(20, 436);
-            this.lvCalcTagList.Name = "lvCalcTagList";
-            this.lvCalcTagList.OwnerDraw = true;
-            this.lvCalcTagList.Size = new System.Drawing.Size(1316, 175);
-            this.lvCalcTagList.TabIndex = 1;
-            this.lvCalcTagList.UseCompatibleStateImageBehavior = false;
-            this.lvCalcTagList.View = System.Windows.Forms.View.Details;
-            // 
-            // lvTagList
-            // 
-            this.lvTagList.FullRowSelect = true;
-            this.lvTagList.GridLines = true;
-            this.lvTagList.Location = new System.Drawing.Point(20, 81);
-            this.lvTagList.Name = "lvTagList";
-            this.lvTagList.Size = new System.Drawing.Size(1316, 295);
-            this.lvTagList.TabIndex = 0;
-            this.lvTagList.UseCompatibleStateImageBehavior = false;
-            this.lvTagList.View = System.Windows.Forms.View.Details;
-            this.lvTagList.DrawColumnHeader += new System.Windows.Forms.DrawListViewColumnHeaderEventHandler(this.lvTagList_DrawColumnHeader);
-            this.lvTagList.DoubleClick += new System.EventHandler(this.lvTagList_DoubleClick);
-            // 
             // btnTagRemove
             // 
             this.btnTagRemove.BackgroundImage = global::IEW.GatewayService.Properties.Resources.minus2;
@@ -283,7 +221,7 @@
             this.btnTagRemove.FlatAppearance.BorderSize = 0;
             this.btnTagRemove.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnTagRemove.Font = new System.Drawing.Font("新細明體", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.btnTagRemove.Location = new System.Drawing.Point(1318, 951);
+            this.btnTagRemove.Location = new System.Drawing.Point(1318, 645);
             this.btnTagRemove.Name = "btnTagRemove";
             this.btnTagRemove.Size = new System.Drawing.Size(45, 45);
             this.btnTagRemove.TabIndex = 22;
@@ -297,7 +235,7 @@
             this.btnTagAdd.FlatAppearance.BorderSize = 0;
             this.btnTagAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnTagAdd.Font = new System.Drawing.Font("新細明體", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.btnTagAdd.Location = new System.Drawing.Point(1267, 951);
+            this.btnTagAdd.Location = new System.Drawing.Point(1267, 645);
             this.btnTagAdd.Name = "btnTagAdd";
             this.btnTagAdd.Size = new System.Drawing.Size(45, 45);
             this.btnTagAdd.TabIndex = 21;
@@ -307,23 +245,69 @@
             // btnLoadTag
             // 
             this.btnLoadTag.Font = new System.Drawing.Font("Arial Narrow", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLoadTag.Location = new System.Drawing.Point(986, 951);
+            this.btnLoadTag.Location = new System.Drawing.Point(1012, 645);
             this.btnLoadTag.Name = "btnLoadTag";
-            this.btnLoadTag.Size = new System.Drawing.Size(220, 67);
+            this.btnLoadTag.Size = new System.Drawing.Size(220, 51);
             this.btnLoadTag.TabIndex = 23;
             this.btnLoadTag.Text = "Load Tag Template";
             this.btnLoadTag.UseVisualStyleBackColor = true;
             this.btnLoadTag.Click += new System.EventHandler(this.btnLoadTag_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(52, 288);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 31);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "Normal Tag:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(52, 694);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 31);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Calculation Tag:";
+            // 
+            // lvCalcTagList
+            // 
+            this.lvCalcTagList.FullRowSelect = true;
+            this.lvCalcTagList.GridLines = true;
+            this.lvCalcTagList.Location = new System.Drawing.Point(47, 737);
+            this.lvCalcTagList.Name = "lvCalcTagList";
+            this.lvCalcTagList.OwnerDraw = true;
+            this.lvCalcTagList.Size = new System.Drawing.Size(1316, 193);
+            this.lvCalcTagList.TabIndex = 25;
+            this.lvCalcTagList.UseCompatibleStateImageBehavior = false;
+            this.lvCalcTagList.View = System.Windows.Forms.View.Details;
+            // 
+            // lvTagList
+            // 
+            this.lvTagList.FullRowSelect = true;
+            this.lvTagList.GridLines = true;
+            this.lvTagList.Location = new System.Drawing.Point(47, 329);
+            this.lvTagList.Name = "lvTagList";
+            this.lvTagList.Size = new System.Drawing.Size(1316, 295);
+            this.lvTagList.TabIndex = 24;
+            this.lvTagList.UseCompatibleStateImageBehavior = false;
+            this.lvTagList.View = System.Windows.Forms.View.Details;
             // 
             // frmEditDevice
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(192F, 192F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(1414, 1046);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lvCalcTagList);
+            this.Controls.Add(this.lvTagList);
             this.Controls.Add(this.btnLoadTag);
             this.Controls.Add(this.btnTagRemove);
             this.Controls.Add(this.btnTagAdd);
-            this.Controls.Add(this.gbTagSet);
             this.Controls.Add(this.pnlPLC);
             this.Controls.Add(this.btnDeviceCancel);
             this.Controls.Add(this.btnDeviceSave);
@@ -340,8 +324,6 @@
             this.pnlBLE.PerformLayout();
             this.pnlPLC.ResumeLayout(false);
             this.pnlPLC.PerformLayout();
-            this.gbTagSet.ResumeLayout(false);
-            this.gbTagSet.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -365,13 +347,12 @@
         private System.Windows.Forms.Button btnDeviceSave;
         private System.Windows.Forms.Panel pnlBLE;
         private System.Windows.Forms.Panel pnlPLC;
-        private System.Windows.Forms.GroupBox gbTagSet;
-        private System.Windows.Forms.ListView lvTagList;
         private System.Windows.Forms.Button btnTagRemove;
         private System.Windows.Forms.Button btnTagAdd;
         private System.Windows.Forms.Button btnLoadTag;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ListView lvCalcTagList;
+        private System.Windows.Forms.ListView lvTagList;
     }
 }
